8587664a190b98ce910659f6eb3d5f5619c22b64
Julien VIDAL <julien.vidal@fortil.group>
2024-01-30 15:26:40 +0100

    update welcome-page

--- front-projet/src/app/components/welcome-page/welcome-page.component.ts
+++ front-projet/src/app/components/welcome-page/welcome-page.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { WelcomePageService } from './welcome-page.service';
 
 @Component({
   selector: 'app-welcome-page',
+  styleUrls: ['./welcome-page.component.css'],
   template: `
-  <h1>Bienvenue sur la page d'accueil Fortil !</h1>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.816942207706!2d1.4871702748301419!3d43.58952915654861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aea3405d835ec7%3A0xcc7d9c8b9a636c34!2sFortil%20Toulouse!5e0!3m2!1sfr!2sfr!4v1701875925006!5m2!1sfr!2sfr" width="500" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <div nz-row>
+    <div class="left-divider" nz-col nzSpan="6">
+      <div class="weather-card">
+        <div class="weather-info">
+          <img class="weather-logo" [src]="image_url" alt="Logo Météo">
+          <div class="weather-details" *ngIf="meteoToulouseDatas">
+            <h3>Météo {{ meteoToulouseDatas.name }}</h3>
+            <h4>{{ meteoToulouseDatas.weather[0].description | titlecase}} - {{ meteoToulouseDatas.main.temp }} °C</h4>
+            <h5>Min : {{ meteoToulouseDatas.main.temp_min }} - Max : {{ meteoToulouseDatas.main.temp_max }}</h5>
+          </div>
+        </div>
+      </div>
+      <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11559.268567100306!2d1.489745!3d43.589525!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aea3405d835ec7%3A0xcc7d9c8b9a636c34!2sFortil%20Toulouse!5e0!3m2!1sfr!2sfr!4v1706623027050!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+    <div nz-col nzSpan="17" nzOffset="1">
+        <h1>Bienvenue sur la plate-forme de l'agence Fortil SO !</h1>
+        <img class="welcome-img" src="assets/images/img-welcome-page.jpg" alt="">
+        <p>Postez vos annonces (services, covoiturages, dons ou ventes...) directement via la rubrique <b>"ANNONCES"</b>.</p>
+        <p>Vous pouvez aussi réserver votre bureau pour une date souhaitée dans vos locaux de Balma (2 Rue Jean Giono, 31130 Balma) via la rubrique <b>"RESERVATION BUREAU"</b>.</p>
+    </div>
+  </div>
 `,
 })
-export class WelcomePageComponent {}
+export class WelcomePageComponent {
+
+  meteoToulouseDatas!: any
+  image_url!: any
+
+  constructor(
+    private welcomePageService: WelcomePageService, 
+  ) {}
+
+  ngOnInit() {
+    this.getMeteoToulouse()
+    // console.log(this.meteoToulouseDatas.weather[0].description)
+  }
+
+  getMeteoToulouse() {
+    this.welcomePageService.getMeteoToulouse()
+    .subscribe((response: any) => {
+      this.meteoToulouseDatas = response,
+      this.image_url = `https://openweathermap.org/img/wn/${this.meteoToulouseDatas.weather[0].icon}@2x.png`
+
+      // if (response.status != 201) {
+      // } else if (response.status === 201) {
+      // }
+    })
+  }
+
+
+}
---
+++ front-projet/src/app/components/welcome-page/welcome-page.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WelcomePageService {
+
+  constructor(private http: HttpClient) { }
+
+  getMeteoToulouse(): Observable<{}> {
+    const params = new HttpParams() //Construction des paramètres de la requête
+    .set('token', 'e7e0e21af2f0feb780e416e039e9c67ecd657fa001143551499466fdb64e83d3') //token de l'API
+    .set('insee', '31555'); //code insee de la ville de Toulouse
+    const options = {   // Options formatées pour la requête HTTP
+      params: params
+    };
+    // return this.http.get(`https://api.meteo-concept.com/api/forecast/daily/0`, options).pipe(
+    
+    const ApiKey = '63b86ccc6d55c58d81196cac6a504f01'
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=Toulouse&appid=${ApiKey}&lang=fr&units=metric`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    )
+  }
+
+  
+  private log(response: any) {
+    console.table(response)
+  }
+
+  private handleError(error: Error, errorValue: any) { 
+    console.log(error)
+    return of(errorValue) //on retourne Observable vide si erreur
+  }
+}
